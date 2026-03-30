1e8a1d587e36df9de4df202f54f4bb5a80ccf2d5
speedsixwheels <it@speedsixwheels.com>
2026-03-30 08:48:28 +0200

    dev

--- wp-content/themes/hello-elementor-child/page-registres.php
+++ wp-content/themes/hello-elementor-child/page-registres.php
@@ -31,7 +31,7 @@ get_header();
                 // Botón para descargar PDF centrado
                 echo '<div style="display: flex; justify-content: center; align-items: center; margin: 40px auto; max-width: 600px; text-align: center;">
                     <div>
-                        <a target="_blank" href="/wp-content/themes/hello-elementor-child/generar-pdf/" class="btn-descargar-pdf" style="display: inline-block; background-color: #D04840; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; font-weight: bold; transition: background-color 0.3s; box-shadow: 0 2px 8px rgba(208, 72, 132, 0.3);">
+                        <a target="_blank" href="/wp-content/themes/hello-elementor-child/generar-pdf/"  style="display: inline-block; background-color: #D04840; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; font-weight: bold; transition: background-color 0.3s; box-shadow: 0 2px 8px rgba(208, 72, 132, 0.3);">
                             📄 Crear PDF de signatures
                         </a>
                         <p style="margin-top: 15px; color: #000; font-size: 14px;">Total de signatures: ' . $data_form['total_found'] . '</p>  
