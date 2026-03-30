38568d6f75fc18029923dd205b0f45409f4e3821
speedsixwheels <it@speedsixwheels.com>
2026-03-30 09:26:49 +0200

    dev

--- wp-content/themes/hello-elementor-child/page-registres.php
+++ wp-content/themes/hello-elementor-child/page-registres.php
@@ -21,6 +21,7 @@ get_header();
                 'meses' => 12,
             ];
             $data_form = get_data_curl($url,$post_fields);  
+           
         
             
            
@@ -34,7 +35,7 @@ get_header();
                         <a target="_blank" href="/wp-content/themes/hello-elementor-child/generar-pdf/"  style="display: inline-block; background-color: #D04840; color: white; padding: 15px 30px; text-decoration: none; border-radius: 5px; font-weight: bold; transition: background-color 0.3s; box-shadow: 0 2px 8px rgba(208, 72, 132, 0.3);">
                             📄 Crear PDF de signatures
                         </a>
-                        <p style="margin-top: 15px; color: #000; font-size: 14px;">Total de signatures: ' . $data_form['total_found'] . '</p>  
+                        <p style="margin-top: 15px; color: #000; font-size: 14px;">Total de signatures: ' . $data_form['total_entries'] . '</p>  
                     </div>
                 </div>';
                 
